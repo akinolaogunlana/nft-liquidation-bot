3a8b5fca8feec1370533b5be0fa95804b4be6e40
akinolaogunlana <142597105+akinolaogunlana@users.noreply.github.com>
2025-07-24 10:54:43 +0100

    Update liquidationTest.js

--- test/liquidationTest.js
+++ test/liquidationTest.js
@@ -1,1 +1,23 @@
-test/
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("LiquidationBot", function () {
+  it("should initiate a flash loan and liquidate NFT", async function () {
+    const [deployer] = await ethers.getSigners();
+
+    const Bot = await ethers.getContractFactory("LiquidationBot");
+    const bot = await Bot.deploy("0x...AaveProvider"); // replace with actual address
+
+    await bot.updateTargets("0x...NFTContract", "0x...Marketplace");
+
+    const tx = await bot.requestFlashLoan(
+      "0x...WETH", // token
+      ethers.utils.parseEther("10"), // amount
+      "0x...BorrowerAddress", // NFT borrower
+      1234 // Token ID
+    );
+
+    await tx.wait();
+    console.log("Flash loan requested");
+  });
+});
