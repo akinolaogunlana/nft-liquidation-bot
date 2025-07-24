a7bd1308d076033cf624812ccb7fcf005450da1e
akinolaogunlana <142597105+akinolaogunlana@users.noreply.github.com>
2025-07-24 10:56:29 +0100

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,1 +1,12 @@
-# Hardhat config
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.20",
+  networks: {
+    mainnet: {
+      url: process.env.MAINNET_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
+};
